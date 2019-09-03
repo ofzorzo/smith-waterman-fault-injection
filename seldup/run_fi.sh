db39d68ae248b218bc26d0bd69006c3c0647eecb
Felipe Pereira <felipe.1707@hotmail.com>
2019-09-03 11:21:45 -0700

    Seldup folder cleaned up

--- seldup/run_fi.sh
+++ seldup/run_fi.sh
@@ -1,10 +1,11 @@
 FOLDER=seldup
 for i in 1 2 3 4
 do
-	make > dump
+	#make > dump
+	make
 	cd ../carol-fi
 	rm -rf logs
-	./fault_injector.py -c ../seldup/smith_waterman.conf -i 500 -d /tmp/smith_waterman/detected.log
+	./fault_injector.py -c ../seldup/smith_waterman.conf -i 50 -d /tmp/smith_waterman/detected.log
 	
 	for i in $(ls logs); do
 		rsync -av logs/$i/* ../$FOLDER/logs/$i/ > dump
@@ -13,5 +14,5 @@ do
 	cd ../$FOLDER
 	./faultinj_parser.py logs
 	mv $FOLDER_*.csv faultReport
-	rm -rf dump
+	#rm -rf dump
 done
--- seldup/shell_test.sh
+++
@@ -1,5 +0,0 @@
-FOLDER=seldup
-for i in 1 2 3 4 5 6 7 8 9 10 11 12
-do
-	echo "Teste numero $i"
-done
